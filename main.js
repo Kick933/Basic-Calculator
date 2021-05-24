7c008dad1053273273d56e138133452d0db34adb
Pavitar Kumar <pavitrsheoran@gmail.com>
2021-05-24 16:12:20 +0530

    added event listeners to numbers

---
+++ main.js
@@ -0,0 +1,43 @@
+const show = document.querySelector("#display-area");
+ 
+let temp = "0"
+const one = document.querySelector('#one')
+one.addEventListener('click',()=>{
+    show.textContent += "1";
+})
+const two = document.querySelector("#two")
+two.addEventListener('click',()=>{
+    show.textContent += "2";
+})
+const three = document.querySelector("#three")
+three.addEventListener('click',()=>{
+    show.textContent += "3";
+})
+const four = document.querySelector("#four")
+four.addEventListener('click',()=>{
+    show.textContent += "4";
+})
+const five = document.querySelector("#five")
+five.addEventListener('click',()=>{
+    show.textContent += "5";
+})
+const six = document.querySelector("#six")
+six.addEventListener('click',()=>{
+    show.textContent += "6";
+})
+const seven = document.querySelector("#seven")
+seven.addEventListener('click',()=>{
+    show.textContent += "7";
+})
+const eight = document.querySelector("#eight")
+eight.addEventListener('click',()=>{
+    show.textContent += "8";
+})
+const nine = document.querySelector("#nine")
+one.addEventListener('click',()=>{
+    show.textContent += "9";
+})
+const aDot = document.querySelector("#dot")
+one.addEventListener('click',()=>{
+    show.textContent += ".";
+})
