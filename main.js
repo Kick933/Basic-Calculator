737ea3fb127483a26b33239ad5329dc6c5726d77
Pavitar Kumar <pavitrsheoran@gmail.com>
2021-05-27 12:27:16 +0530

    Completedworking model.

--- main.js
+++ main.js
@@ -2,14 +2,13 @@ const show = document.querySelector("#display-area");
 
 let temp_delete = "";
 let delete_string1 = "";
-let delete_string2 = "";
-let temp_dot = 0;
-let dot1 = temp_dot;
-let dot2 =0;
-let opr1_length=0;
-let opr2_length=0;
+let dot = 0;
 let temp_length = 0;
-
+let opr1 = 0;
+let opr2 = 0;
+let operator = "";
+let operatorBeingDisplayed = 0;
+let secondOperandAvailable = 0;
 // Displays zero on display.
 function display_zero(){
     show.textContent = "0"
@@ -21,6 +20,11 @@ function display_nothing(){
 // Event listeners for each number.
 const one = document.querySelector('#one')
 one.addEventListener('click',()=>{
+    if(operatorBeingDisplayed == 1){
+        display_nothing();
+        operatorBeingDisplayed =0;
+        secondOperandAvailable = 1;
+    }
     if(temp_length == 5){
         alert("Maximum limit reached");
         return;
@@ -28,11 +32,16 @@ one.addEventListener('click',()=>{
     if(temp_length == 0){
         display_nothing();
     }
-    show.textContent += "1";
+    show.textContent += 1;
     temp_length++;
 })
 const two = document.querySelector("#two")
 two.addEventListener('click',()=>{
+    if(operatorBeingDisplayed == 1){
+        display_nothing();
+        operatorBeingDisplayed =0;
+        secondOperandAvailable = 1;
+    }
     if(temp_length == 5){
         alert("Maximum limit reached");
         return;
@@ -40,11 +49,16 @@ two.addEventListener('click',()=>{
     if(temp_length == 0){
         display_nothing();
     }
-    show.textContent += "2";
+    show.textContent += 2;
     temp_length++;
 })
 const three = document.querySelector("#three")
 three.addEventListener('click',()=>{
+    if(operatorBeingDisplayed == 1){
+        display_nothing();
+        operatorBeingDisplayed =0;
+        secondOperandAvailable = 1;
+    }
     if(temp_length == 5){
         alert("Maximum limit reached");
         return;
@@ -52,11 +66,16 @@ three.addEventListener('click',()=>{
     if(temp_length == 0){
         display_nothing();
     }
-    show.textContent += "3";
+    show.textContent += 3;
     temp_length++;
 })
 const four = document.querySelector("#four")
 four.addEventListener('click',()=>{
+    if(operatorBeingDisplayed == 1){
+        display_nothing();
+        operatorBeingDisplayed =0;
+        secondOperandAvailable = 1;
+    }
     if(temp_length == 5){
         alert("Maximum limit reached");
         return;
@@ -69,6 +88,11 @@ four.addEventListener('click',()=>{
 })
 const five = document.querySelector("#five")
 five.addEventListener('click',()=>{
+    if(operatorBeingDisplayed == 1){
+        display_nothing();
+        operatorBeingDisplayed =0;
+        secondOperandAvailable = 1;
+    }
     if(temp_length == 5){
         alert("Maximum limit reached");
         return;
@@ -81,6 +105,11 @@ five.addEventListener('click',()=>{
 })
 const six = document.querySelector("#six")
 six.addEventListener('click',()=>{
+    if(operatorBeingDisplayed == 1){
+        display_nothing();
+        operatorBeingDisplayed =0;
+        secondOperandAvailable = 1;
+    }
     if(temp_length == 5){
         alert("Maximum limit reached");
         return;
@@ -93,6 +122,11 @@ six.addEventListener('click',()=>{
 })
 const seven = document.querySelector("#seven")
 seven.addEventListener('click',()=>{
+    if(operatorBeingDisplayed == 1){
+        display_nothing();
+        operatorBeingDisplayed =0;
+        secondOperandAvailable = 1;
+    }
     if(temp_length == 5){
         alert("Maximum limit reached");
         return;
@@ -105,6 +139,11 @@ seven.addEventListener('click',()=>{
 })
 const eight = document.querySelector("#eight")
 eight.addEventListener('click',()=>{
+    if(operatorBeingDisplayed == 1){
+        display_nothing();
+        operatorBeingDisplayed =0;
+        secondOperandAvailable = 1;
+    }
     if(temp_length == 5){
         alert("Maximum limit reached");
         return;
@@ -117,6 +156,11 @@ eight.addEventListener('click',()=>{
 })
 const nine = document.querySelector("#nine")
 nine.addEventListener('click',()=>{
+    if(operatorBeingDisplayed == 1){
+        display_nothing();
+        operatorBeingDisplayed =0;
+        secondOperandAvailable = 1;
+    }
     if(temp_length == 5){
         alert("Maximum limit reached");
         return;
@@ -132,9 +176,9 @@ nine.addEventListener('click',()=>{
 // Handles usage of dot for first operand.
 const aDot = document.querySelector("#dot")
 aDot.addEventListener('click',()=>{
-    if(temp_dot == 0){
+    if(dot == 0){
     show.textContent += ".";
-    dot1++;
+    dot++;
     }else{
         alert("Dot allowed only once.");
     }
@@ -144,12 +188,7 @@ aDot.addEventListener('click',()=>{
 const clear = document.querySelector("#clear-btn")
 clear.addEventListener('click',()=>{
     display_zero();
-    temp_dot = 0;
-    dot1 = temp_dot;
-    dot2 =0;
-    opr1_length=0;
-    opr2_length=0;
-    temp_length = 0;
+    dot = 0;
 })
 // Handles working of "DELETE" button.
 const btnDelete = document.querySelector("#delete-btn")
@@ -162,4 +201,95 @@ btnDelete.addEventListener('click',()=>{
     show.textContent = delete_string1;
     temp_length--;
     }
+})
+
+//
+//
+//
+//
+// The event listeners handle operator buttons.
+//
+//
+//
+//
+
+const addition = document.querySelector("#add");
+addition.addEventListener('click', ()=>{
+    if(operatorBeingDisplayed == 0){
+    opr1 = show.textContent;
+    temp_length = 0;
+    dot = 0
+    show.textContent = "+";
+    operator = "add";
+    operatorBeingDisplayed = 1;
+    }else{
+        operator = "add";
+        show.textContent = "+";
+        operatorBeingDisplayed = 1;
+    }
+})
+const subtraction = document.querySelector("#subtract");
+subtraction.addEventListener('click', ()=>{
+    if(operatorBeingDisplayed == 0){
+    opr1 = show.textContent;
+    temp_length = 0;
+    dot = 0;
+    show.textContent = "-";
+    operator = "subtract";
+    operatorBeingDisplayed = 1;
+    }else{
+        operator = "subtract";
+        show.textContent = "-";
+        operatorBeingDisplayed = 1;
+    }
+})
+const multiplication = document.querySelector("#multiply");
+multiplication.addEventListener('click', ()=>{
+    if(operatorBeingDisplayed ==0){
+    opr1 = show.textContent;
+    temp_length = 0;
+    dot = 0;
+    show.textContent = "X";
+    operator = "multiply";
+    operatorBeingDisplayed = 1;
+    }else{
+        operator = "multiply";
+        operatorBeingDisplayed = 1;
+        show.textContent = "X";
+    }
+})
+const division = document.querySelector("#divide");
+division.addEventListener('click', ()=>{
+    if(operatorBeingDisplayed ==0){
+    opr1 = show.textContent;
+    temp_length = 0;
+    dot = 0;
+    show.textContent = "/";
+    operator = "divide";
+    operatorBeingDisplayed = 1;
+    }else{
+        operator = "divide";
+        operatorBeingDisplayed = 1;
+        show.textContent = '/';
+    }
+})
+
+const equal = document.querySelector("#equal")
+equal.addEventListener('click',()=>{{
+        opr2 = show.textContent;
+    if(operator == "add"){
+        show.textContent = parseFloat(opr1) + parseFloat(opr2);
+    }else if(operator == "subtract"){
+        show.textContent = opr1 - opr2;
+    }else if(operator == "divide"){
+        if(opr2 == 0){
+            alert("Can't divide by zero");
+        }else{
+            show.textContent = opr1 / opr2;
+        };
+    }else if(operator == "multiply"){
+        show.textContent = opr1 * opr2;
+    }
+    opr1 = show.textContent;
+}
 })
